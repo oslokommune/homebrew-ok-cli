d7723c058d39ed3a61f345ad06c9ce4b6bd27f19
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2022-04-08 13:35:39 +0200

    correct URL

--- ok.rb
+++ ok.rb
@@ -3,15 +3,15 @@
 
 class Ok < Formula
   desc "okctl2-poc testing"
-  homepage "https://github.com/yngvark/okctl2-poc"
-  version "0.0.4"
+  homepage "https://github.com/oslokommune/ok-cli-poc"
+  version "0.0.1"
   license "AGPL-3.0-only"
 
   depends_on "hashicorp/tap/terraform"
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/yngvark/okctl2-poc/releases/download/0.0.4/ok"
+      url "https://github.com/oslokommune/ok-cli-poc/releases/download/0.0.1/ok"
       sha256 "502502adce76ae35b3222e1c518588780f76d8d52aba60d567add6c5f95a420d"
 
       def install
